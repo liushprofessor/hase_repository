a37683afe817591898cb366003dd34cfa5d46344
Liush <442549007@qq.com>
2020-08-07 16:33:40 +0800

    添加kafka实例去幂等生产者

--- kafka/src/main/java/com/liu/ProductOnce.java
+++ kafka/src/main/java/com/liu/ProductOnce.java
@@ -7,6 +7,8 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Liush
@@ -20,6 +22,7 @@ public class ProductOnce {
 
     public static void main(String[] args) {
 
+        CountDownLatch countDownLatch=new CountDownLatch(50000);
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.159.128:9092");
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
@@ -27,23 +30,87 @@ public class ProductOnce {
         //去幂等
         properties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
-        ProducerRecord<String, String> record = new ProducerRecord<>("test", "name", "liu10");
-        ProducerRecord<String, String> record1 = new ProducerRecord<>("test", "name", "liu10");
-        ProducerRecord<String, String> record2 = new ProducerRecord<>("test", "name", "liu11");
+        ThreadPoolExecutor threadPoolExecutor=new MyThreadPool().createThreadPoolExecutor();
+        AtomicInteger id=new AtomicInteger();
+        threadPoolExecutor.execute(new MyTask(id,producer,countDownLatch));
+        threadPoolExecutor.execute(new MyTask(id,producer,countDownLatch));
+        threadPoolExecutor.execute(new MyTask(id,producer,countDownLatch));
         try {
-            RecordMetadata metadata = producer.send(record).get();
-            producer.send(record1);
-            producer.send(record2);
-            System.out.println(metadata);
-        } catch (Exception e) {
+            countDownLatch.await();
+            System.out.println("执行结束"+id.get());
+            System.out.println("countdown_____"+countDownLatch.getCount());
+        } catch (InterruptedException e) {
             e.printStackTrace();
-
-
         }
 
 
 
     	}
+
+}
+
+
+        class MyTask implements Runnable{
+
+
+            private AtomicInteger id;
+
+            private KafkaProducer<String, String> producer;
+
+            private CountDownLatch countDownLatch;
+
+            public MyTask(AtomicInteger id, KafkaProducer<String, String> producer, CountDownLatch countDownLatch) {
+                this.id = id;
+                this.producer = producer;
+                this.countDownLatch = countDownLatch;
+            }
+
+            @Override
+            public void run() {
+                int num=id.incrementAndGet();
+                System.out.println("任务开始..................");
+                while (num<1000000){
+                    num=id.incrementAndGet();
+                    ProducerRecord<String, String> record = new ProducerRecord<>("test", "name", id.incrementAndGet()+"");
+                    producer.send(record);
+                    countDownLatch.countDown();
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+            @Override
+            public String toString() {
+                return "任务id是 "+id;
+            }
+        }
+
+
+
+    class MyThreadPool{
+
+
+        public ThreadPoolExecutor createThreadPoolExecutor(){
+
+            BlockingDeque blockingDeque=new LinkedBlockingDeque(1);
+            ThreadPoolExecutor pool=new ThreadPoolExecutor(1,1,0, TimeUnit.SECONDS,blockingDeque);
+                pool.setRejectedExecutionHandler((r,executor)->{
+                    System.out.println(r.toString());
+                    System.out.println("队列已满,丢弃任务");
+
+                });
+
+            return pool;
+
+    }
+
+
+
+
 
 
 
